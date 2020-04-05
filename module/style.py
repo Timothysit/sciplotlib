86a836ed33337e61576b9f4ad8d7ea9bc061ec7d
timsit <sitpakhang@gmail.com>
2020-04-05 22:52:34 +0100

    added using of latex sans math font

--- module/style.py
+++ module/style.py
@@ -1,5 +1,8 @@
 import sciplotlib as spl
 import os
+import matplotlib as mpl
+from collections import OrderedDict
+
 
 def get_style(stylesheet_name='nature'):
     """
@@ -28,4 +31,58 @@ def get_style(stylesheet_name='nature'):
     stylesheet_path = os.path.join(dirname, stylesheet_name + '.mplstyle')
 
     return stylesheet_path
+
+
+def use_sans_maths(return_type='permenant', fig=None, ax=None, verbose=False):
+    """
+    Uses LaTeX computer modern *SANS* mathematics font.
+    Note that you can reset using: plt.rcdefaults()
+
+    Parameters
+    ----------
+    return_type : (str)
+    fig : (matplotlib fig object)
+    ax : (matplotlib ax object)
+    verbose : (bool)
+    """
+
+    if return_type is 'temp_dict' and (fig is None):
+        # usage is with mpl.rc_context(use_sans_maths())
+        # note that there is currenlty an issue with matplotli that prevents this from working without
+        # calling plt.show() or fig.savefig()
+        # see: https://github.com/matplotlib/matplotlib/issues/13431
+        if verbose:
+            print("""Note you must use plt.show() so that the plot will render properly. 
+                    You can turn off this warning by setting verbose=False.""")
+        rc_param_dict = {'text.usetex': True,
+                         'text.latex.preamble': [r'\usepackage[cm]{sfmath}'],
+                         'font.family': 'sans-serif',
+                         'font.sans-serif': 'cm',
+                         'text.latex.preamble': [
+                             r'\usepackage{siunitx}',
+                             r'\sisetup{detect-all}',
+                             r'\usepackage{sansmath}',
+                             r'\sansmath'
+                         ]}
+
+        return OrderedDict(rc_param_dict.items())
+
+    elif return_type is 'permenant':
+        mpl.rcParams['text.usetex'] = True
+        mpl.rcParams['text.latex.preamble'] = [r'\usepackage[cm]{sfmath}']
+        mpl.rcParams['font.family'] = 'sans-serif'
+        mpl.rcParams['font.sans-serif'] = 'cm'
+        mpl.rcParams['text.latex.preamble'] = [
+            r'\usepackage{siunitx}',  # i need upright \micro symbols, but you need...
+            r'\sisetup{detect-all}',  # ...this to force siunitx to actually use your fonts
+            r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
+            r'\sansmath'  # <- tricky! -- gotta actually tell tex to use!
+        ]
+
+    if (fig is not None) and (ax is not None):
+        # TODO: see if there is a way to redraw (fig.canvas.draw()?) to circumvent the context manager
+        fig = fig
+        ax = ax
+
+        return fig, ax
 
